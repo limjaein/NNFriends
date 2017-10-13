6950ecdda08b917b513f1057e8d4b4ebc9bcad86
Unknown <victory9287@naver.com>
2017-10-13 19:06:26 +0900

    테스트

--- app/src/main/java/com/android/project/nnfriends_/MenuActivity.java
+++ app/src/main/java/com/android/project/nnfriends_/MenuActivity.java
@@ -3,6 +3,7 @@ package com.android.project.nnfriends_;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,6 +11,8 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Toast.makeText(this, "a", Toast.LENGTH_SHORT).show();
 
     // Example of a call to a native method
     TextView tv = (TextView) findViewById(R.id.sample_text);
